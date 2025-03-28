4c2da5bc3207ec880522884d445810ba03de7eb6
drharshpratap <dr.harshpratap@gmail.com>
2025-03-28 22:31:50 +0530

    added default route

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jhelum-oms-ui/', // Ensure this matches your repository name
   build: {
     outDir: 'dist'
   }
-})
+});
