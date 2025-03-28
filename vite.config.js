cfcef1f9ec418c7730d914cc9e5497939487647e
drharshpratap <dr.harshpratap@gmail.com>
2025-03-28 22:27:01 +0530

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/jhelum-oms-ui/', // Ensure this matches your repository name
   build: {
     outDir: 'dist'
   }
